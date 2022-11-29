9d1c4ffd407afc457eee5cbe06ce9805aa42302e
Mikolaj <nogamikolaj00@gmail.com>
2022-11-29 17:46:02 +0100

    test

--- src/main/java/pl/edu/pjwstk/ARC2/checkimage/CheckImageController.java
+++ src/main/java/pl/edu/pjwstk/ARC2/checkimage/CheckImageController.java
@@ -3,6 +3,7 @@ package pl.edu.pjwstk.ARC2.checkimage;
 import com.google.gson.JsonObject;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,7 @@ import org.json.*;
 @AllArgsConstructor
 public class CheckImageController {
 
-    @PostMapping("/onNewImage")
+    @PostMapping(value = "/onNewImage", consumes = MediaType.APPLICATION_JSON_VALUE)
     public HttpStatus checkImage(@RequestBody JSONObject object){
         System.out.println(object);
         return HttpStatus.OK;
