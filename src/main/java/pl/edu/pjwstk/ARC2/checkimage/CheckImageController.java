4b7e0f5a1d04e304247ccbcdad65e02abe9ab007
Mikolaj <nogamikolaj00@gmail.com>
2022-11-29 18:16:14 +0100

    test

--- src/main/java/pl/edu/pjwstk/ARC2/checkimage/CheckImageController.java
+++ src/main/java/pl/edu/pjwstk/ARC2/checkimage/CheckImageController.java
@@ -1,21 +1,32 @@
 package pl.edu.pjwstk.ARC2.checkimage;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.JsonObject;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.json.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Base64;
+
 @RestController
 @AllArgsConstructor
 public class CheckImageController {
 
-    @PostMapping(value = "/onNewImage", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public HttpStatus checkImage(@RequestBody JSONObject object){
-        System.out.println(object.keySet());
+    private final ObjectMapper objectMapper;
+
+    @PostMapping(value = "/onNewImage")
+    public HttpStatus checkImage(HttpServletRequest req) throws IOException {
+        JsonNode bodyNode = objectMapper.readTree(req.getReader());
+
+        String base64Data = bodyNode.get("message")
+                .get("attributes").get("objectId").asText();
+        System.out.println(base64Data);
         return HttpStatus.OK;
     }
 }
