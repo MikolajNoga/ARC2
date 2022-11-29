ee4db7244a8d3b0fad795c791df19bd758ee7af4
Mikolaj <nogamikolaj00@gmail.com>
2022-11-29 18:21:47 +0100

    test

--- src/main/java/pl/edu/pjwstk/ARC2/checkimage/CheckImageController.java
+++ src/main/java/pl/edu/pjwstk/ARC2/checkimage/CheckImageController.java
@@ -5,10 +5,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.JsonObject;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.json.*;
+import pl.edu.pjwstk.ARC2.checkimage.visionapi.DetectLabelsGcs;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
@@ -20,13 +22,18 @@ public class CheckImageController {
 
     private final ObjectMapper objectMapper;
 
-    @PostMapping(value = "/onNewImage")
+    @PostMapping(
+            value = "/onNewImage",
+            consumes = MediaType.APPLICATION_JSON_VALUE)
     public HttpStatus checkImage(HttpServletRequest req) throws IOException {
         JsonNode bodyNode = objectMapper.readTree(req.getReader());
 
         String base64Data = bodyNode.get("message")
                 .get("attributes").get("objectId").asText();
-        System.out.println(base64Data);
+        DetectLabelsGcs.detectLabelsGcs("gs://project-arc2.appspot.com/" + base64Data);
+
+
+
         return HttpStatus.OK;
     }
 }
